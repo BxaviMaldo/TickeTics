2023cb650d35ec2d0800a28852aba2bfe542b396
damarispenafiel2023-rgb <damarispenafiel2023@gmail.com>
2026-06-27 14:12:16 -0500

    debug: log usuarioActual

--- frontend/src/paginas/admin/GestionUsuarios.jsx
+++ frontend/src/paginas/admin/GestionUsuarios.jsx
@@ -33,6 +33,7 @@ const sm = {
 
 const GestionUsuarios = () => {
   const { usuario: usuarioActual } = useAutenticacion();
+  console.log('usuarioActual:', usuarioActual);
   const [usuarios, setUsuarios] = useState([]);
   const [mostrarForm, setMostrarForm] = useState(false);
   const [form, setForm] = useState({ nombre: '', correo: '', rol: 'cliente' });
